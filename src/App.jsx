98ef7ccf858fa9e8ab2d97078d177cf12e9dfe9d
Luca1791 <demagluca1791@gmail.com>
2024-10-18 13:47:40 +0200

    Deployment 3.0

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Footer from './Footer/Footer';
 
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
@@ -21,24 +20,6 @@ function App() {
     <Footer></Footer>
     
     
-    
-      {/* <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
